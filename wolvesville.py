3b264713df5cb4ce69def7b9beeb4512d48ef4f3
RedScorpion <38502084+capRS@users.noreply.github.com>
2021-06-07 16:10:09 +0300

    Update wolvesville.py

--- wolvesville.py
+++ wolvesville.py
@@ -1,7 +1,8 @@
+# this code is made and distributed by RedScorpion
 # need help? contact me on discord  RedScorpion#5785
-# for visual toturial, this script is explained on youtube, open this link on your browser
+# for visual toturial, open this link on your browser
 # https://www.youtube.com/watch?v=QEoIjWfHf2c
-# full documentation on how to use this code is on git hub, open this link on your browser
+# for documentation, open this link on your browser
 # https://github.com/capRS/wolves/
 
 import pyautogui as gui
@@ -24,20 +25,19 @@ def click(x, y):
 
 # finds the watch video button, clicks if it is there
 def find_button():
-    coords = (1718, 651)
+    coords = (mom, dad)
     for m in range(1000):
         if gui.pixel(coords[0], coords[1])[0] == 255:
             click(coords[0], coords[1])
             break
         else:
             coords = (coords[0], coords[1] - 5)
-            gui.moveTo(coords[0], coords[1])
 
 
 def rich_overnight():
     find_button()
     time.sleep(numpy.random.randint(40, 43))
-    click(1898, 533)
+    click(me, you) # clicks the back button
     time.sleep(numpy.random.randint(5, 7))
     find_button()
     time.sleep(numpy.random.randint(16, 17))
